256f758fe9f41cda9199df5b8a2077eec2ca20e6
Artem Abalmaz <128359894+ItiraelQQ@users.noreply.github.com>
2024-10-15 14:36:21 +0300

    Added CRUD operations

---
+++ BookLibraryAPI/AppDbContext.cs
@@ -0,0 +1,23 @@
+﻿using BookLibraryAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookLibraryAPI
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Book> Books { get; set; }
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseNpgsql("name=DefaultConnection");
+            }
+        }
+    }
+}
